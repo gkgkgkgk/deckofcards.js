230395ded0a3a56e6fb4c499e8967a5430b11a18
gkgkgkgk <gkepets@gmail.com>
2018-02-15 19:35:59 -0500

    Added JS and collapsibility

---
+++ main.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+	$( ".card" ).click(function() {
+		if($(this).hasClass("collapse")){
+			$(this).removeClass( "collapse" );
+			
+		}
+		else{
+			$(this).addClass("collapse");
+			//$(this).removeClass( "card" );
+		}
+	});
+
+
+});
